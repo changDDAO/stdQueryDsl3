389bfee55795743efc7cd7f65394990dc66c2504
Changhogod <younch8342@naver.com>
2023-09-06 18:33:10 +0900

    use querydsl QueryProjection and dynamic search

--- src/test/java/com/changddao/demo/QueryDslBasicTest.java
+++ src/test/java/com/changddao/demo/QueryDslBasicTest.java
@@ -1,14 +1,24 @@
 package com.changddao.demo;
 
+import com.changddao.demo.dto.MemberDto;
+import com.changddao.demo.dto.QMemberDto;
+import com.changddao.demo.dto.UserDto;
 import com.changddao.demo.entity.Member;
 import com.changddao.demo.entity.QMember;
 import com.changddao.demo.entity.QTeam;
 import com.changddao.demo.entity.Team;
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
+import com.querydsl.core.types.Predicate;
+import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.types.dsl.CaseBuilder;
+import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.PersistenceUnit;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -26,6 +36,9 @@ import static org.assertj.core.api.Assertions.*;
 public class QueryDslBasicTest {
     @PersistenceContext
     EntityManager em;
+
+    @PersistenceUnit
+    EntityManagerFactory emf;
     JPAQueryFactory queryFactory;
 
     @BeforeEach
@@ -185,5 +198,219 @@ public class QueryDslBasicTest {
         assertThat(teamA.get(member.age.avg())).isEqualTo(15);
 
     }
+    /*
+    * 팀A에 소속된 모든 회원을 조회
+    * */
+    @Test
+    public void join(){
+    //given
+        List<Member> aMembers = queryFactory.select(member)
+                .from(member)
+                .join(member.team, team)
+                .where(team.name.eq("teamA"))
+                .fetch();
+
+
+        //when
+    assertThat(aMembers).extracting("username")
+            .containsExactly("member1", "member2");
+
+
+
+    //then
+
+
+    }
+    @Test
+    public void fetchJoinNo(){
+    //given
+    em.flush();
+    em.clear();
+
+        Member findMember = queryFactory.selectFrom(member)
+                .where(member.username.eq("member1"))
+                .fetchOne();
+
+
+        //when
+        boolean loaded = emf.getPersistenceUnitUtil().isLoaded(findMember.getTeam());
+        assertThat(loaded).as("페치조인 미적용").isFalse();
+
+
+        //then
+
+
+    }
+    @Test
+    public void fetchJoin(){
+    //given
+        Member findMember = queryFactory.selectFrom(member)
+                .join(member.team, team).fetchJoin()
+                .where(member.username.eq("member1"))
+                .fetchOne();
+
+
+        //when
+        boolean loaded = emf.getPersistenceUnitUtil().isLoaded(findMember.getTeam());
+
+
+        //then
+        assertThat(loaded).isTrue();
+
+    }
+
+    /*
+    * 나이가 가장많은 회원 조회
+    * */
+    @Test
+    public void subQuery(){
+    //given
+        QMember memberSub = new QMember("memberSub");
+        List<Member> result = queryFactory.selectFrom(member)
+                .where(member.age.eq(
+                        JPAExpressions.select(memberSub.age.max())
+                                .from(memberSub)
+
+                )).fetch();
+
+
+        //when
+        int age = result.get(0).getAge();
+        //then
+    assertThat(age).isEqualTo(40);
+
+    }
+
+    /*
+    * 나이가 평균 이상인 회원 조회
+    * */
+    @Test
+    public void subQuery2(){
+        QMember subMember = new QMember("subMember");
+    //given
+        List<Member> result = queryFactory.selectFrom(member)
+                .where(member.age.goe(
+                        JPAExpressions.select(subMember.age.avg())
+                                .from(subMember)
+
+                )).fetch();
+
+
+        //when
+        for (Member member1 : result) {
+            System.out.println(member1);
+        }
+
+
+    //then
+
+
+    }
+    //case when절
+    @Test
+    public void complexCase(){
+    //given
+        List<String> result = queryFactory.select(new CaseBuilder()
+                        .when(member.age.between(10, 20)).then("10~20살")
+                        .when(member.age.between(21, 30)).then("21~30살")
+                        .otherwise("기타")
+                ).from(member)
+                .fetch();
+
+
+        //when
+        for (String s : result) {
+            System.out.println("s = " + s);
+        }
+
+
+    //then
+
+
+    }
+    /*dto로 조회하기*/
+    @Test
+    public void jpqlDto(){
+    //given
+        List<MemberDto> result = em.createQuery("select new com.changddao.demo.dto.MemberDto(m.username, m.age)" +
+                "from Member m", MemberDto.class).getResultList();
+
+
+        //when
+        for (MemberDto memberDto : result) {
+            System.out.println("memberDto = " + memberDto);
+        }
+
+
+    //then
+
+
+    }
+    @Test
+    public void findByConstructor(){
+    //given
+        List<UserDto> result = queryFactory.select(Projections.constructor(UserDto.class,
+                        member.username, member.age))
+                .from(member)
+                .fetch();
+
+
+        //when
+        for (UserDto userDto : result) {
+            System.out.println("userDto = " + userDto);
+        }
+
+
+    //then
+
+
+    }
+    @Test
+    public void queryProjection(){
+    //given
+        List<MemberDto> result = queryFactory.select(new QMemberDto(member.username, member.age))
+                .from(member)
+                .fetch();
+
+
+        //when
+        for (MemberDto memberDto : result) {
+            System.out.println("memberDto = " + memberDto);
+        }
+
+
+    //then
+    }
+    @Test
+    public void dynamicQUery_WhereParam(){
+    //given
+    String usernameParam ="member1";
+    Integer ageParam =null;
+
+    List<Member> result = searchMember2(usernameParam, ageParam);
+    //when
+        for (Member member1 : result) {
+            System.out.println("member1 = " + member1);
+        }
+
+
+    //then
+
+
+    }
+    private List<Member> searchMember2(String usernameCond, Integer ageCond) {
+        return queryFactory.selectFrom(member)
+                .where(usernameEq(usernameCond), ageEq(ageCond))
+                .fetch();
+    }
+
+    private BooleanExpression ageEq(Integer ageCond) {
+        return ageCond==null? null : member.age.eq(ageCond);
+    }
+
+    private BooleanExpression usernameEq(String usernameCond) {
+        return usernameCond==null? null: member.username.eq(usernameCond);
+    }
+
 
 }
