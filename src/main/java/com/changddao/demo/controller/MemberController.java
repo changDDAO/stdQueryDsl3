6036ee0e99608d843b6f617a51b45252c229fdae
Changhogod <younch8342@naver.com>
2023-10-06 16:22:26 +0900

    paging test use spring data jpa

--- src/main/java/com/changddao/demo/controller/MemberController.java
+++ src/main/java/com/changddao/demo/controller/MemberController.java
@@ -3,7 +3,10 @@ package com.changddao.demo.controller;
 import com.changddao.demo.dto.MemberSearchCondition;
 import com.changddao.demo.dto.MemberTeamDto;
 import com.changddao.demo.repository.MemberJpaRepository;
+import com.changddao.demo.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,9 +16,19 @@ import java.util.List;
 @RequiredArgsConstructor
 public class MemberController {
     private final MemberJpaRepository memberJpaRepository;
+    private final MemberRepository memberRepository;
 
     @GetMapping("/v1/members")
     public List<MemberTeamDto> searchMemberV1(MemberSearchCondition condition) {
         return memberJpaRepository.searchbyBuilder(condition);
     }
+    @GetMapping("/v2/members")
+    public Page<MemberTeamDto> searchMemberV2(MemberSearchCondition condition, Pageable pageable) {
+        return memberRepository.searchPageSimple(condition, pageable);
+    }
+    @GetMapping("/v3/members")
+    public Page<MemberTeamDto> searchMemberV3(MemberSearchCondition condition,Pageable pageable) {
+        return memberRepository.searchPageComplex(condition, pageable);
+    }
+
 }
